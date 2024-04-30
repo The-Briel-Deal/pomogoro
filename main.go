e3c5df86f83b2614b245b781ec3f7647ce8bcc4b
Gabriel Ford <gabe@fordltc.net>
2024-04-30 01:05:14 -0400

    My Pomodoro timer is very happy now!

--- main.go
+++ main.go
@@ -2,20 +2,32 @@ package main
 
 import "fmt"
 import "os"
+import "os/exec"
 import "time"
+import "strconv"
 
 import "github.com/charmbracelet/bubbles/timer"
 import tea "github.com/charmbracelet/bubbletea"
 
-const timeout = time.Second * 5
-
 type model struct {
-	timer timer.Model
+	timer          timer.Model
+	specialMessage string
 }
 
 func initialModel() model {
+	timeoutMin := 25
+	if len(os.Args) > 1 {
+		arg1 := os.Args[1]
+		arg1i, err := strconv.Atoi(arg1)
+		if err == nil {
+			timeoutMin = arg1i
+		}
+	}
+	timeout := time.Duration(timeoutMin) * time.Minute
+
 	return model{
-		timer: timer.NewWithInterval(timeout, time.Millisecond),
+		timer:          timer.NewWithInterval(timeout, time.Millisecond*100),
+		specialMessage: "Press Q to quit!",
 	}
 }
 
@@ -29,17 +41,29 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		var cmd tea.Cmd
 		m.timer, cmd = m.timer.Update(msg)
 		return m, cmd
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			m.specialMessage = "Goodbye (:"
+			return m, tea.Quit
+		}
 	}
 	return m, nil
 }
 
 func (m model) View() string {
-	s := m.timer.View()
+	bs, err := exec.Command("/usr/bin/figlet", "-fslant", m.timer.View()).Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	s := string(bs)
 
 	if m.timer.Timedout() {
 		s = "All done!"
 	}
 	s += "\n"
+	s += m.specialMessage + "\n"
 	return s
 }
 
